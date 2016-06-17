2380484715a707c8e66d7698227366711506ea37
Daniel Bendel <daniel.bendel@gmail.com>
2016-06-17 10:39:46 +0200

    chore(sample): migrate to using slots

--- src/grid/column.js
+++ src/grid/column.js
@@ -1,11 +1,11 @@
 import {inlineView, customElement, children, constants, generateBindables, Util} from '../common/common';
 
-@inlineView('<template><content></content></template>')
+@inlineView('<template><slot></slot></template>')
 @customElement(`${constants.elementPrefix}column`)
 @generateBindables('columns', ['clipMode', 'allowEditing', 'allowFiltering', 'allowGrouping', 'allowSorting', 'allowResizing', 'commands', 'cssClass', 'customAttributes', 'dataSource', 'defaultValue', 'disableHtmlEncode', 'displayAsCheckBox', 'editParams', 'editTemplate', 'editType', 'field', 'foreignKeyField', 'foreignKeyValue', 'format', 'headerTemplateID', 'headerText', 'headerTextAlign', 'isFrozen', 'isIdentity', 'isPrimaryKey', 'showInColumnChooser', 'template', 'textAlign', 'tooltip', 'type', 'validationRules', 'visible', 'width'])
 
 export class Column {
-  @children(`${constants.elementPrefix}template`) template
+  @children(`${constants.elementPrefix}template`) template = [];
   bind() {
     if (this.template[0]) {
       let util = new Util();
@@ -13,4 +13,3 @@ export class Column {
     }
   }
 }
-
